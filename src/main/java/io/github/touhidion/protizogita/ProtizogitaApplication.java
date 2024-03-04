bfccd750a90fc4be54f72f18d12b259798c3cbc5
touhidion <touhid.mti@gmail.com>
2024-03-04 16:36:51 +0600

    init project

---
+++ src/main/java/io/github/touhidion/protizogita/ProtizogitaApplication.java
@@ -0,0 +1,13 @@
+package io.github.touhidion.protizogita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtizogitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtizogitaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/touhidion/protizogita/ProtizogitaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.touhidion.protizogita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtizogitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
